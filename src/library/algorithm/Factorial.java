3e65f66761fbdfd16b5f5965dd1ad9dba8440050
Yuta Nishino <ynishino.omoikane@gmail.com>
2018-05-14 10:59:04 +0900

    Factorial

---
+++ src/library/algorithm/Factorial.java
@@ -0,0 +1,21 @@
+package library.algorithm;
+
+class Factorial {
+    long[] fillFactorial(int max, int mod) {
+        long[] facts = new long[max + 1];
+        facts[0] = 1;
+        for (int i = 1; i < facts.length; i++) {
+            facts[i] = facts[i - 1] * i % mod;
+        }
+        return facts;
+    }
+
+    long[] fillFactorial(int max) {
+        long[] facts = new long[max + 1];
+        facts[0] = 1;
+        for (int i = 1; i < facts.length; i++) {
+            facts[i] = facts[i - 1] * i;
+        }
+        return facts;
+    }
+}
