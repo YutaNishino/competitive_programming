648483f4db9e1def234e418f41e298ba5f5f9432
Yuta Nishino <ynishino.omoikane@gmail.com>
2018-05-15 21:30:31 +0900

    ARC087ABCD

---
+++ src/atcoder/ARC087/ARC087A.java
@@ -0,0 +1,84 @@
+package atcoder.ARC087;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ARC087A {
+    int a, b;
+
+    public static void main(String args[]) {
+        new ARC087A().run();
+    }
+
+    void run() {
+        FastReader sc = new FastReader();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        solve();
+    }
+
+    void solve() {
+        int sum = a + b;
+        if (sum % 2 == 0) {
+            System.out.println(sum / 2);
+        } else {
+            System.out.println(sum / 2 + 1);
+        }
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
