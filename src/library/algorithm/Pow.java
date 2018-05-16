c741a68bd47aa21aeb7599ee8f0bb23378d9df28
Yuta Nishino <ynishino.omoikane@gmail.com>
2018-05-17 01:33:42 +0900

    Pow in O(log n)

---
+++ src/library/algorithm/Pow.java
@@ -0,0 +1,15 @@
+package library.algorithm;
+
+public class Pow {
+    long pow(long x, int n) {
+        long ans = 1;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ans = ans * x;
+            }
+            x = x * x;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
