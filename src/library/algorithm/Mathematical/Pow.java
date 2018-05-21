b861acc9e2c52d0bad273bc4ad7bd5c5e1b66a8f
Yuta Nishino <ynishino.omoikane@gmail.com>
2018-05-21 19:24:20 +0900

    Pow with mod, inverse with mod

--- src/library/algorithm/Mathematical/Pow.java
+++ src/library/algorithm/Mathematical/Pow.java
@@ -12,4 +12,20 @@ public class Pow {
         }
         return ans;
     }
+
+    long pow(long x, int n, int MOD) {
+        long ans = 1;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ans = ans * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
+        }
+        return ans;
+    }
+
+    long inverse(long num, int mod) {
+        return pow(num, mod - 2, mod);
+    }
 }
