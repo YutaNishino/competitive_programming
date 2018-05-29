a65a536d2fd793bcb281588569f1eda2bc75cdae
Yuta Nishino <ynishino.omoikane@gmail.com>
2018-05-30 00:19:37 +0900

    ABC009CD

--- src/atcoder/ABC009/ABC009C.java
+++ src/atcoder/ABC009/ABC009C.java
@@ -35,9 +35,9 @@ public class ABC009C {
         }
         int count = 0;
         char[] ans = new char[cs.length];
-        for (int i = 0; i < n; i++) {
+        int i;
+        for (i = 0; i < n; i++) {
             char first = tMap.firstKey();
-            //System.out.println(cs[i] + " " + first);
             if (first == cs[i]) {
                 if (sMap.get(first) == 1) {
                     sMap.remove(first);
@@ -49,65 +49,60 @@ public class ABC009C {
                 } else {
                     tMap.put(first, tMap.get(first) - 1);
                 }
+                ans[i] = first;
             } else {
-                count++;
-                if (sMap.get(first) == 1) {
-                    sMap.remove(first);
-                } else {
-                    sMap.put(first, sMap.get(first) - 1);
+                boolean flag = false;
+                for (Map.Entry<Character, Integer> entry : tMap.entrySet()) {
+                    int misMatch = cs.length - i - 1;
+                    char c;
+                    for (int j = 0; j < 26; j++) {
+                        c = (char)(j + 'a');
+                        int sNum = sMap.getOrDefault(c, 0);
+                        int tNum = tMap.getOrDefault(c, 0);
+                        if (c == cs[i]) {
+                            sNum--;
+                        }
+                        if (c == entry.getKey()) {
+                            tNum--;
+                        }
+                        misMatch -= Math.min(sNum, tNum);
+                    }
+                    if (count + 1 + misMatch <= k) {
+                        flag = true;
+                        ans[i] = entry.getKey();
+                        if (entry.getKey() != cs[i]) {
+                            count++;
+                        }
+                        if (sMap.get(cs[i]) == 1) {
+                            sMap.remove(cs[i]);
+                        } else {
+                            sMap.put(cs[i], sMap.get(cs[i]) - 1);
+                        }
+                        if (tMap.get(entry.getKey()) == 1) {
+                            tMap.remove(entry.getKey());
+                        } else {
+                            tMap.put(entry.getKey(), tMap.get(entry.getKey()) - 1);
+                        }
+                        break;
+                    }
                 }
-                if (tMap.get(first) == 1) {
-                    tMap.remove(first);
-                } else {
-                    tMap.put(first, tMap.get(first) - 1);
-                }
-                int misMatch = cs.length - i - 1;
-                for (int j = 0; j < 26; j++) {
-                    int sNum = sMap.getOrDefault((char)(j + 'a'), 0);
-                    int tNum = tMap.getOrDefault((char)(j + 'a'), 0);
-                    misMatch -= Math.min(sNum, tNum);
-                }
-                //System.out.println(misMatch);
-                if (count + 1 + misMatch > k) {
-                    if (!sMap.containsKey(first)) {
-                        sMap.put(first, 1);
+                if (!flag) {
+                    ans[i] = cs[i];
+                    if (sMap.get(cs[i]) == 1) {
+                        sMap.remove(cs[i]);
                     } else {
-                        sMap.put(first, sMap.get(first) + 1);
+                        sMap.put(cs[i], sMap.get(cs[i]) - 1);
                     }
-                    for (int j = i; j < cs.length; j++) {
-                        if (sMap.containsKey(cs[j])) {
-                            ans[j] = cs[j];
-                            if (sMap.get(cs[j]) == 1) {
-                                sMap.remove(cs[j]);
-                            } else {
-                                sMap.put(cs[j], sMap.get(cs[j]) - 1);
-                             }
-                        }
+                    if (tMap.get(cs[i]) == 1) {
+                        tMap.remove(cs[i]);
+                    } else {
+                        tMap.put(cs[i], tMap.get(cs[i]) - 1);
                     }
-                    //System.out.println(Arrays.toString(ans));
-                    for (int j = i; j < cs.length; j++) {
-                        if (ans[j] == 0) {
-                            char next = sMap.firstKey();
-                            ans[j] = next;
-                            if (sMap.get(next) == 1) {
-                                sMap.remove(next);
-                            } else {
-                                sMap.put(next, sMap.get(next) - 1);
-                            }
-                        }
-                    }
-                    for (int j = 0; j < cs.length; j++) {
-                        System.out.print(ans[j]);
-                    }
-                    System.out.println();
-                    return;
                 }
-
             }
-            ans[i] = first;
         }
-        for (int i = 0; i < cs.length; i++) {
-            System.out.print(ans[i]);
+        for (int j = 0; j < cs.length; j++) {
+            System.out.print(ans[j]);
         }
         System.out.println();
     }
--- src/atcoder/ABC009/ABC009D.java
+++ src/atcoder/ABC009/ABC009D.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class ABC009D {
-    int n, m;
+    int k, m;
+    long[] as, cs;
 
     public static void main(String args[]) {
         new ABC009D().run();
@@ -14,13 +15,78 @@ public class ABC009D {
 
     void run() {
         FastReader sc = new FastReader();
-        n = sc.nextInt();
+        k = sc.nextInt();
         m = sc.nextInt();
+        as = new long[k];
+        for (int i = k - 1; i >= 0; i--) {
+            as[i] = sc.nextLong();
+        }
+        cs = new long[k];
+        for (int i = 0; i < k; i++) {
+            cs[i] = sc.nextLong();
+        }
         solve();
     }
 
     void solve() {
+        if (m <= k) {
+            System.out.println(as[k - m]);
+            return;
+        }
+        long[][][] doubling = new long[31][k][k];
+        for (int i = 0; i < k; i++) {
+            doubling[0][0][i] = cs[i];
+        }
+        for (int i = 1; i < k; i++) {
+            doubling[0][i][i - 1] = (1L << 32) - 1;
+        }
+        for (int i = 1; i < 31; i++) {
+            doubling[i] = multiplyMatrices(doubling[i - 1], doubling[i - 1]);
+        }
+        m -= k;
+        System.out.println(multiply(dfs(m, doubling), as)[0]);
+;   }
 
+    long[][] dfs(int n, long[][][] doubling) {
+        if (n == 0) {
+            long[][] id = new long[doubling[0].length][doubling[0][0].length];
+            for (int i = 0; i < id.length; i++) {
+                id[i][i] = (1L << 32) - 1;
+            }
+            return id;
+        }
+        if (n == 1) {
+            return doubling[0];
+        }
+        int l = 1;
+        int power = 0;
+        while (l <= n) {
+            l *= 2;
+            power++;
+        }
+        return multiplyMatrices(doubling[--power], dfs(n - l / 2, doubling));
+    }
+
+    long[][] multiplyMatrices(long[][] a, long[][] b) {
+        long[][] c = new long[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                for (int k = 0; k < a.length; k++) {
+                    c[i][j] ^= a[i][k] & b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+
+    long[] multiply(long[][] a, long[] v) {
+        long[] v2 = new long[v.length];
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length; j++) {
+                v2[i] ^= a[i][j] & v[j];
+            }
+        }
+        return v2;
     }
 
     static class FastReader {
